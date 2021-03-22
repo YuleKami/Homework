feff397fe9df4d1cfc06d76c5d88131583dbda28
YuleKami <Malceew@yandex.ru>
2021-03-22 17:39:55 +0700

    vue-3

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,10 +5,14 @@
     <div class="product-container">
       <div class="product-image">
         <img :src="image">
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
 
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
+        <span v-if="inSale">Купить по скидке: {{sale}}</span>
+        <span v-else>Купить</span>
         <p>{{ description }}</p>
         <a :href="link">More products like this</a>
 
@@ -29,7 +33,10 @@
         //image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
         image: " https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg",
         altText: 'A pair of socks',
-        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks.'
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks.',
+        inStock: true,
+        inSale: true,
+        sale: '50%',
       };
     },
   };
