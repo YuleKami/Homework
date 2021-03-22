7b199abf9315a227a746e0e380b24b12a1373a0d
YuleKami <Malceew@yandex.ru>
2021-03-22 16:24:21 +0700

    vue1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,57 @@
 <template>
-  <div>
+
+  <div class="product">
+    <div class="product-image"></div>
+    <div class="product-info">
+      <h1>{{ product }}</h1>
+      <p>{{ description }}</p>
+    </div>
   </div>
+
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  export default {
+    name: 'App',
+    components: {
+    },
+    data() {
+      return {
+        product: 'Socks',
+        description: 'A pair of warm fuzzy socks',
+        image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+      };
+    },
+  };
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  body {
+    font-family: tahoma;
+    color: #282828;
+    margin: 0px;
+  }
+
+  .product {
+    display: flex;
+  }
+
+  img {
+    border: 1px solid #d8d8d8;
+    width: 70%;
+    margin: 40px;
+    box-shadow: 0px .5px 1px #d8d8d8;
+  }
+
+  .product-image {
+    flex-basis: 700px;
+  }
+
+  .product-info {
+    margin-top: 10px;
+    flex-basis: 500px;
+  }
+
 </style>
