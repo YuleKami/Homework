049bc0dd5ea123e0a1ced2bb79382ffc37f30933
YuleKami <Malceew@yandex.ru>
2021-04-05 12:10:39 +0700

    vue-4

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,6 +11,13 @@
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
+        <ul v-for="variant in variants" :key="variant.variantId">
+          <li>{{variant.color}}</li>
+        </ul>
+        <ul v-for="size in sizes" :key="size.sizeId">
+          <li>{{size}}</li>
+        </ul>
+        <div v-for="detail in details" :key="detail.detailId">{{detail}}</div>
         <span v-if="inSale">Купить по скидке: {{sale}}</span>
         <span v-else>Купить</span>
         <p>{{ description }}</p>
@@ -32,6 +39,18 @@
         description: 'A pair of warm fuzzy socks',
         //image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
         image: " https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg",
+        details: ['50% cotton', '30% wool', '20% polyester'],
+        sizes: ['S','M', 'L', 'XL'],
+        variants: [
+          {
+            id: 2234,
+            color: 'green'
+          },
+          {
+            id: 2235,
+            color: 'blue'
+          }
+        ],
         altText: 'A pair of socks',
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks.',
         inStock: true,
